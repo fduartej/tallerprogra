3fafaf128b029b3c4f704148cb918ae9bb6240b9
Fred <fduartej@gmail.com>
2018-11-18 21:30:50 -0500

    Clases Complejas

---
+++ src/tallerprogra/fundamental/Flight.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallerprogra.fundamental;
+
+/**
+ *
+ * @author fred
+ */
+public class Flight {
+    
+    private int seats;
+    private int passengers;
+    
+    public Flight(){
+        seats=150;
+        passengers=0;
+    }
+    
+    public static void main(String[] args){
+        Flight objFlight = new Flight();
+        objFlight.addPassenger();
+        objFlight.summary();
+        
+        for(int i=0;i<100;i++){
+            objFlight.addPassenger();
+            objFlight.summary();
+        }
+    
+    }
+    
+    public void addPassenger(){
+        if(passengers<seats)
+           passengers++;
+    }
+
+    public void summary(){
+        System.out.printf("Total asientos %d \n", seats );
+        System.out.printf("Total pasajeros %d \n", passengers );
+        System.out.printf("Total asientos disponibles %d \n", seats - passengers );
+    }
+    
+    public int getSeats() {
+        return seats;
+    }
+
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    
+    
+}
